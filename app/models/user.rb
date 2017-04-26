c1fd2686291853820ca656cb78e034b8e826bc8e
mxdavis <md01@me.com>
2017-04-26 03:13:58 +0300

    didnt help

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,6 @@ class User < ActiveRecord::Base
   validates_presence_of :name, :password_digest
 
   include Slugifiable::InstanceMethods
-  # extend Slugifiable::ClassMethods
-
-  def self.find_by_slug(slug)
-    self.all.detect do |s|
-      s.name.downcase == slug.gsub("-", " ")
-    end
-  end
+  extend Slugifiable::ClassMethods
 
 end
